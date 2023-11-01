7f19094f3b50a37b123e7d777e6df047c0407975
IdrisVeli1987 <idrisveliev@gmail.com>
2023-11-01 17:05:55 +0400

    add Routing and ErrorPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Home from './Pages/Home'
+import React from "react";
+import Routing from "./Components/Routing";
 
 const App = () => {
   return (
     <div>
-      <Home/>
+      <Routing />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
