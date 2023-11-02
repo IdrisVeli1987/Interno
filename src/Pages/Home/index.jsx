832f1f5186a241ecc853839f42c6c1b45e21c2f6
IdrisVeli1987 <idrisveliev@gmail.com>
2023-11-02 15:53:21 +0400

    add Banner to Home page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navigation from "../../Components/Navigation";
+import Banner from "../../Components/Banner";
 
 const Home = () => {
   return (
     <div>
       <Navigation />
+      <Banner />
     </div>
   );
 };
