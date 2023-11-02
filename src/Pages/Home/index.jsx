92e14f0897d695a64ec7333eddb90b4c265cc21c
IdrisVeli1987 <idrisveliev@gmail.com>
2023-11-02 20:23:05 +0400

    add WorkPlanCards to Home Page

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navigation from "../../Components/Navigation";
 import Banner from "../../Components/Banner";
+import WorkPlanCards from "../../Components/WorkPlanCards";
 
 const Home = () => {
   return (
     <div>
       <Navigation />
       <Banner />
+      <WorkPlanCards />
     </div>
   );
 };
