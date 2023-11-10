5fc3e11dfdbbbd610a2b5d9b7a702d2d209fc8ec
IdrisVeli1987 <idrisveliev@gmail.com>
2023-11-10 17:14:45 +0400

    add motion framer

--- src/Components/StaticNumbers/index.jsx
+++ src/Components/StaticNumbers/index.jsx
@@ -3,8 +3,39 @@ import styles from "./style.module.css";
 import { useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
+import { animate, useMotionValue, useTransform } from "framer-motion";
 
 const URL = "http://localhost:3000/StaticNumbers";
+
+// export function Counter({ from, to }) {
+//   const nodeRef = useRef();
+
+//   useEffect(() => {
+//     const node = nodeRef.current;
+
+//     const controls = animate(from, to, {
+//       duration: 1,
+//       onUpdate(value) {
+//         node.textContent = value.toFixed(2);
+//       },
+//     });
+
+//     return () => controls.stop();
+//   }, [from, to]);
+
+//   return <p ref={nodeRef} />;
+// }
+
+// const count = useMotionValue(0);
+// const rounded = useTransform(count, (latest) => Math.round(latest));
+
+// useEffect(() => {
+//   const controls = animate(count, 100);
+
+//   return controls.stop;
+// }, []);
+
+// return <motion.div>{rounded}</motion.div>;
 
 const StaticNumbers = () => {
   const [numbers, setNumbers] = useState([]);
@@ -19,10 +50,15 @@ const StaticNumbers = () => {
     <>
       <div className={`${styles.staticBox} `}>
         <div className="container">
-          <div className={`${styles.statics} col d-flex align-items-center justify-content-center py-5 gap-3 mt-5 mb-5`}>
+          <div
+            className={`${styles.statics} col d-flex align-items-center justify-content-center py-5 gap-3 mt-5 mb-5`}
+          >
             {numbers.map(({ id, number, description }) => {
               return (
-                <div key={id} className={`${styles.staticNumber} ${styles.line}`}>
+                <div
+                  key={id}
+                  className={`${styles.staticNumber} ${styles.line}`}
+                >
                   <h6 className={`${styles.staticNbr}`}>{number}</h6>
                   <p className={styles.staticDesc}>{description}</p>
                 </div>
