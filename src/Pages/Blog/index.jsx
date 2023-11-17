83654279e54737befc70cbe49b0ee39e30429afe
IdrisVeli1987 <idrisveliev@gmail.com>
2023-11-17 11:18:15 +0400

    add Blog page and Image

--- src/Pages/Blog/index.jsx
+++ src/Pages/Blog/index.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import Navigation from '../../Components/Navigation'
+import React from "react";
+import Navigation from "../../Components/Navigation";
+import { motion } from "framer-motion";
+import { Helmet } from "react-helmet";
+import PageBanner from "../../Components/PageBanner";
 
 const Blog = () => {
   return (
     <div>
-      <Navigation/>
-    </div>
-  )
-}
+      <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+        <Helmet>
+          <title></title>
+        </Helmet>
 
-export default Blog
+        <Navigation />
+        <PageBanner
+          src="../Blog/Blog_Banner.svg"
+          page="Blog"
+          title="Articles & News"
+        />
+        {/* <h2>{project.title}</h2>
+        <p>{project.description}</p> */}
+      </motion.div>
+    </div>
+  );
+};
+
+export default Blog;
