bea01243ccbbb8931bf9bd465b1054f690f8d207
IdrisVeli1987 <idrisveliev@gmail.com>
2023-11-17 17:52:11 +0400

    add media screen

--- src/Components/ArticlesNews/index.jsx
+++ src/Components/ArticlesNews/index.jsx
@@ -19,7 +19,9 @@ const ArticlesNews = () => {
   return (
     <div className="container">
       <div className="row mb-5">
-        <div className={styles.ArticleHeader}>
+        <div
+          className={`${styles.ArticleHeader} col-xl-12 col-md-12 col-sm-12`}
+        >
           <h3>Articles & News</h3>
           <p>
             It is a long established fact that a reader will be distracted by
@@ -27,19 +29,17 @@ const ArticlesNews = () => {
             layouts the points of using.
           </p>
         </div>
-        <div
-          className={`${styles.ArticleContent} d-flex align-items-center mt-5`}
-        >
-          {article.map(({ id, src, title, date }) => (
+        <div className={`${styles.ArticleContent}  mt-5 w-10`}>
+          {article.map(({ id, src, title, date, imageTitle }) => (
             <div
               key={id}
               className={`${styles.ArticleBox} ${
                 id === 2 && styles["ArticleBox-active"]
-              }`}
+              } col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4`}
             >
-              <div className={styles.ArticleBoxImg}>
+              <div className={`${styles.ArticleBoxImg}`}>
                 <img src={src} alt="image" />
-                <span className={styles.text}>Text</span>
+                <span className={styles.text}>{imageTitle}</span>
               </div>
               <h5 className={styles.ImageSubtitle}>{title}</h5>
               <div className={styles.ImageDesc}>
