a2268e25b572dd311968eaa1224120febc06c904
IdrisVeli1987 <idrisveliev@gmail.com>
2023-11-19 13:38:38 +0400

    add copyright

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
   return (
     <div className="container">
       <div className={`${styles.FooterContent} mt-5`}>
-        <div className="col-6">
+        <div className={`${styles.FooterLogo} `}>
           <Link to="/">
             <img
               className={`${styles.footerLogo} mb-2`}
@@ -22,7 +22,7 @@ const Footer = () => {
             It is a long established fact that a reader <br /> will be
             distracted lookings.
           </p>
-          <div className={`${styles.SocialIcons}`}>
+          <div className={`${styles.SocialIcons} `}>
             <Link to="http://Facebook.com">
               <FaFacebookF />
             </Link>
@@ -37,7 +37,57 @@ const Footer = () => {
             </Link>
           </div>
         </div>
+        <div className={`${styles.pages}`}>
+          <h4>Pages</h4>
+          <Link to="/about">
+            <p>About</p>
+          </Link>
+          <Link to="/OurProjects">
+            <p>Our Projects</p>
+          </Link>
+          <Link to="/#">
+            <p>Our Team</p>
+          </Link>
+          <Link to="/#">
+            <p>Contact Us</p>
+          </Link>
+          <Link to="/services">
+            <p>Services</p>
+          </Link>
+        </div>
+        <div className={`${styles.Services}`}>
+          <h4>Services</h4>
+          <Link to="#">
+            <p>Kitchan</p>
+          </Link>
+          <Link to="#">
+            <p>Liviing Area</p>
+          </Link>
+          <Link to="#">
+            <p>Bathroom</p>
+          </Link>
+          <Link to="#">
+            <p>Dining Hall</p>
+          </Link>
+          <Link to="#">
+            <p>Bedroom</p>
+          </Link>
+        </div>
+        <div className={`${styles.Contact}`}>
+          <h4>Contact</h4>
+          <p>
+            55 East Birchwood Ave. <br /> Brooklyn, New York 11201
+          </p>
+          <p>idrisveliev@gmail.com</p>
+          <p>(+994) 77-233-93-31</p>
+        </div>
       </div>
+        <div className={`${styles.Copyright}`}>
+          <p>
+            Copyright © Interno | Designed by Victorflow Templates - Powered by
+            Webflow
+          </p>
+        </div>
     </div>
   );
 };
