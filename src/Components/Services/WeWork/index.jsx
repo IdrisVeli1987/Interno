8657ea2584c4a46e5a1ab6bfb5e14bbe07d8d978
IdrisVeli1987 <idrisveliev@gmail.com>
2023-11-23 20:17:40 +0400

    add WeWork component and styles

--- src/Components/Services/WeWork/index.jsx
+++ src/Components/Services/WeWork/index.jsx
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./style.module.css";
+import axios from "axios";
+
+const url = "http://localhost:3000/ServicesPageFiles";
 
 const WeWork = () => {
+  const [servicePage, setServicePage] = useState([]);
+
+  useEffect(() => {
+    axios.get(url).then(({ data }) => {
+      setServicePage(data);
+    });
+  }, []);
   return (
     <div className="container">
       <div className="row">
         <div className={styles.WeWorkBox}>
-          <h2 className={styles.WeWorkTitle}>How We Work</h2>
-          <p className={styles.WeWorkSubtitle}>
-            It is a long established fact will be distracted.Lorem Ipsum is
-            simply dummy text of the printing and typesetting industry.
-          </p>
+          <div className={styles.WeWorkTitle}>
+            <h2>How We Work</h2>
+            <p>
+              It is a long established fact will be distracted.Lorem Ipsum is
+              simply dummy text of the printing and typesetting industry.
+            </p>
+          </div>
+          <div className={styles.Works}>
+            {servicePage.length &&
+              servicePage.map(
+                ({ id, src, descriptionTitle, description, icon }, index) => {
+                  return (
+                    <div key={id} className={styles.serviceData}>
+                      <img className={styles.WorkImg} src={src} alt="image" />
+                      <div className={styles.ContentTitle}>
+                        <div className={styles.WorkIcon}>
+                          <h3>{index > 8 ? index + 1 : "0" + (index + 1)}</h3>
+                          <img className={styles.icons} src={icon} alt="" />
+                        </div>
+                        <h5>{descriptionTitle}</h5>
+                        <div className={styles.ContentSubtitle}>
+                          <p>{description}</p>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                }
+              )}
+          </div>
         </div>
       </div>
     </div>
