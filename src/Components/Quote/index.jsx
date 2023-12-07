feeba54996738f6d19e0cc095f753337353b2cf9
IdrisVeli1987 <idrisveliev@gmail.com>
2023-12-07 19:23:25 +0400

    add position

--- src/Components/Quote/index.jsx
+++ src/Components/Quote/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import styles from "./style.module.css";
 import { FaQuoteLeft } from "react-icons/fa";
-import Quote from "../../../public/AboutUs/Quote/Pattern1.svg"
-import Quote from "../../../public/AboutUs/Quote/Pattern2.svg"
+// import Quote from "../../../public/AboutUs/Quote/Pattern1.svg"
+// import Quote from "../../../public/AboutUs/Quote/Pattern2.svg"
 
 const Quote = () => {
   return (
@@ -11,7 +11,7 @@ const Quote = () => {
         <div className={`${styles.AuthorQuotes} col-12 col-xl-12`}>
           <div className={styles.Quote}>
             <FaQuoteLeft />
-            <img src={Quote} alt="" />
+            {/* <img src={Quote} alt="" /> */}
             <h1>
               I like an interior that defies labeling. I don't really want
               someone to walk into a room and know that I did it
