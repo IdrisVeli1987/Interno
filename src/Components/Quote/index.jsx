cdc3518b2208dc51fd460549e4de4af06d83a09c
IdrisVeli1987 <idrisveliev@gmail.com>
2023-12-08 15:46:24 +0400

    add media screen

--- src/Components/Quote/index.jsx
+++ src/Components/Quote/index.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import styles from "./style.module.css";
 import { FaQuoteLeft } from "react-icons/fa";
-// import Quote from "../../../public/AboutUs/Quote/Pattern1.svg"
-// import Quote from "../../../public/AboutUs/Quote/Pattern2.svg"
 
 const Quote = () => {
   return (
     <div className="container">
-      <div className="row">
-        <div className={`${styles.AuthorQuotes} col-12 col-xl-12`}>
-          <div className={styles.Quote}>
+      <div className="row ">
+        <div className={`${styles.AuthorQuotes} `}>
+          <div className={`${styles.Quote}`}>
             <FaQuoteLeft />
-            {/* <img src={Quote} alt="" /> */}
             <h1>
               I like an interior that defies labeling. I don't really want
               someone to walk into a room and know that I did it
