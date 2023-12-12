b21f029504784f7bffd5164aa0eb4fc11f43649a
IdrisVeli1987 <idrisveliev@gmail.com>
2023-12-12 13:40:49 +0400

    fix issue

--- src/Components/AboutUs/Result/index.jsx
+++ src/Components/AboutUs/Result/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import { useState } from "react";
-import styles from "./style.module.css"
+import styles from "./style.module.css";
 
 const url = "http://localhost:3000/Result";
 
@@ -10,29 +10,33 @@ const Result = () => {
 
   useEffect(() => {
     axios.get(url).then(({ data }) => {
-      setPortfolio(data)
+      setPortfolio(data);
     });
   }, []);
   return (
     <div className="container">
       <div className="row">
-        {
-            portfolio.map(({id, src, title, description})=>{
-                return(
-                    <div className={styles.ResultBox} key={id}>
-                        <div className={`${styles.ResultImg} row col-12 col-xl-12`}>
-                            <div className={`${styles.ResultPhoto} row col-6`}>
-                                <img src={src} alt="" />
-                            </div>
-                            <div className={`${styles.ResultText} row col-6`}>
-                                <h3>{title}</h3>
-                                <p>{description}</p>
-                            </div>
-                        </div>
-                    </div>
-                )
-            })
-        }
+        {portfolio.map(({ id, src, title, description }) => {
+          return (
+            <div className={styles.ResultBox} key={id}>
+              <div className={`${styles.ResultImg} row `}>
+                <div className={`${styles.ResultPhoto} col-12 col-xl-6`}>
+                  <div>
+                    <img src={src} alt="" />
+                  </div>
+                </div>
+                <div
+                  className={`${styles.ResultText} col-12 col-xl-6 ps-0 ps-xl-5`}
+                >
+                  <div>
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
