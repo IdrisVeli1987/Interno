d1e9b55b6c277314728500a12aa713d0135cc890
IdrisVeli1987 <idrisveliev@gmail.com>
2023-12-14 18:18:21 +0400

    add PeopleThinks component

--- src/Components/AboutUs/PeopleThinks/index.jsx
+++ src/Components/AboutUs/PeopleThinks/index.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Container, Row } from "react-bootstrap";
+import styles from "./style.module.css";
+
+const url = "http://localhost:3000/PeopleThinks";
 
 const PeopleThinks = () => {
+  const [peopleThinks, setPeopleThinks] = useState([]);
+  useEffect(() => {
+    axios.get(url).then(({ data }) => {
+      setPeopleThinks(data);
+    });
+  }, []);
   return (
-    <div>salam</div>
-  )
-}
+    <div className={`${styles.container}`}>
+      <div className="row">
+        {peopleThinks.map(({ id, src }) => {
+          return (
+            <div key={id} className={styles.PeopleContent}>
+              <div className={styles.peopleThinksTitle}>
+                <h3 className={`${styles.PeopleCardTitle}`}>
+                  What the People Thinks <br /> About Us
+                </h3>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default PeopleThinks
+export default PeopleThinks;
