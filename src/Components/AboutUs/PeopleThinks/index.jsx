b7c87fc7b75f577280bfe48df6bef1b36bb3c282
IdrisVeli1987 <idrisveliev@gmail.com>
2023-12-16 17:57:02 +0400

    delete bootstrap

--- src/Components/AboutUs/PeopleThinks/index.jsx
+++ src/Components/AboutUs/PeopleThinks/index.jsx
@@ -1,11 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import {
-  FaFacebook,
-  FaInstagram,
-  FaLinkedinIn,
-  FaTwitter,
-} from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import styles from "./style.module.css";
 
@@ -22,18 +17,14 @@ const PeopleThinks = () => {
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className={`${styles.PeopleContent}`}>
-          <div
-            className={`${styles.peopleThinksTitle}`}
-          >
+        <div className={`${styles.PeopleContent} col-12 col-xl-12`}>
+          <div className={`${styles.peopleThinksTitle} col-12 col-xl-12 col-lg-8`}>
             <h3 className={`${styles.PeopleCardTitle}`}>
               What the People Thinks <br /> About Us
             </h3>
           </div>
-          <div
-            className={`${styles.PeopleContentImg} `}
-          >
-            <div className={`${styles.EmptyCard}  `}>
+          <div className={`${styles.PeopleContentImg} col-12 col-xl-12 col-lg-8`}>
+            <div className={`${styles.EmptyCard} col-12 col-xl-2 col-lg-4 `}>
               <h3>Nattasha Julie</h3>
               <p>Design, Australia</p>
               <div className={styles.EmpatyCardIcons}>
@@ -52,12 +43,7 @@ const PeopleThinks = () => {
               </div>
             </div>
             {peopleThinks.map(({ id, src }) => (
-              <img
-                key={id}
-                src={src}
-                alt=""
-                className={`${styles.PeopleImageBox} `}
-              />
+              <img key={id} src={src} alt="" className={`${styles.PeopleImageBox} col-12 col-xl-2 col-lg-4`}/>
             ))}
           </div>
         </div>
