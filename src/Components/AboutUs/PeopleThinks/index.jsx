11d70f014fe23ebacdc06df53d65bcd8f48abbf5
IdrisVeli1987 <idrisveliev@gmail.com>
2023-12-18 15:22:20 +0400

    add bootstrap col's and responsive codes

--- src/Components/AboutUs/PeopleThinks/index.jsx
+++ src/Components/AboutUs/PeopleThinks/index.jsx
@@ -1,6 +1,11 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { FaFacebook, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTwitter,
+} from "react-icons/fa";
 import { Link } from "react-router-dom";
 import styles from "./style.module.css";
 
@@ -17,14 +22,14 @@ const PeopleThinks = () => {
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className={`${styles.PeopleContent} col-12 col-xl-12`}>
-          <div className={`${styles.peopleThinksTitle} col-12 col-xl-12 col-lg-8`}>
+        <div className={`${styles.PeopleContent} `}>
+          <div className={`${styles.peopleThinksTitle}`}>
             <h3 className={`${styles.PeopleCardTitle}`}>
               What the People Thinks <br /> About Us
             </h3>
           </div>
-          <div className={`${styles.PeopleContentImg} col-12 col-xl-12 col-lg-8`}>
-            <div className={`${styles.EmptyCard} col-12 col-xl-2 col-lg-4 `}>
+          <div className={`${styles.PeopleContentImg}`}>
+            <div className={`${styles.EmptyCard}`}>
               <h3>Nattasha Julie</h3>
               <p>Design, Australia</p>
               <div className={styles.EmpatyCardIcons}>
@@ -43,7 +48,9 @@ const PeopleThinks = () => {
               </div>
             </div>
             {peopleThinks.map(({ id, src }) => (
-              <img key={id} src={src} alt="" className={`${styles.PeopleImageBox} col-12 col-xl-2 col-lg-4`}/>
+              <div className={`${styles.PeopleImageBox}`}>
+                <img key={id} src={src} alt="" />
+              </div>
             ))}
           </div>
         </div>
