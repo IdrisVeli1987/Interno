efd38eb54d5f849818ebde073fe3add40f9cee4b
IdrisVeli1987 <idrisveliev@gmail.com>
2023-12-19 20:57:14 +0400

    add bootstrap cols

--- src/Components/AboutUs/Forms/index.jsx
+++ src/Components/AboutUs/Forms/index.jsx
@@ -21,14 +21,14 @@ const Forms = () => {
     <div className="container">
       <div className="row">
         <div className={styles.FormBox}>
-          <div className={styles.FormTitle}>
+          <div className={`${styles.FormTitle} row col-12 col-xl-12`}>
             <h3>
               Creative project? Let's have <br /> a productive talk.
             </h3>
           </div>
           <div className={styles.FormContent}>
             <form onSubmit={form.handleSubmit}>
-              <div className={styles.FormInput}>
+              <div className={`${styles.FormInput} row col-12 col-xl-12 col-sm-10`}>
                 <input type="name" name="name" placeholder="Name" required />
                 <input
                   type="email"
@@ -37,15 +37,16 @@ const Forms = () => {
                   required
                 />
               </div>
-              <div className={styles.FormTextarea}>
+              <div className={`${styles.FormTextarea} row col-12 col-xl-12 col-sm-10`}>
                 <textarea
                   type="text"
                   name="message"
                   placeholder="Text"
+                  className="row col-12 col-xl-12 col-sm-2"
                 ></textarea>
               </div>
-              <div className={styles.FormBtn}>
-                <Link to="/contact">
+              <div className={`${styles.FormBtn}`}>
+                <Link to="/contact" className={`${styles.FormLink}`}>
                   <Button
                     title="Send Now"
                     arrow
