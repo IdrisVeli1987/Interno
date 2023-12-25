c36d9b94ce82f369c40933aa37f93608ed5034d5
IdrisVeli1987 <idrisveliev@gmail.com>
2023-12-25 16:36:11 +0400

    add lineContent

--- src/Components/PricingAndPlan/index.jsx
+++ src/Components/PricingAndPlan/index.jsx
@@ -15,26 +15,36 @@ const PricingAndPlan = () => {
   return (
     <div className="container">
       <div className="row my-5">
-        {price.map(({ id, title, currency, price, type, priceList }) => (
-          <div className={`col-md-4`} key={id}>
-            <div className={styles.PriceListBox}>
-              <h5>{title}</h5>
-              <div className={styles.priceListBox}>
-                <div className={styles.PriceData}>
-                  <p>{currency}</p>
-                  <h4>{price}</h4>
+        {price.map(
+          ({ id, popularPlan, title, currency, price, type, priceList }) => (
+            <div className={`col-md-4`} key={id}>
+              <div
+                className={`${styles.PriceListBox} ${
+                  popularPlan && styles.popularPlan
+                }`}
+              >
+                <h5>{title}</h5>
+                <div className={styles.priceListBox}>
+                  <div className={styles.PriceData}>
+                    <p>{currency}</p>
+                    <h4 className={styles.PriceTitle}>{price}</h4>
+                  </div>
+                  <p>{type}</p>
                 </div>
-                <p>{type}</p>
-              </div>
-              <div className={styles.ListData}>
-                <hr />
-                {priceList.map((text, index) => (
-                  <p key={index}> {text}</p>
-                ))}
+                <div className={styles.ListData}>
+                  <div className={`${styles.lineContent}`}>
+                    <p className="mx-auto">Most Popular Plans</p>
+
+                    <div className={styles.line} />
+                  </div>
+                  {priceList.map((text, index) => (
+                    <p key={index}> {text}</p>
+                  ))}
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          )
+        )}
       </div>
     </div>
   );
