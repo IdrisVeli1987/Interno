7884c6d5664d9167eebade1cccf04fc4596cc32a
IdrisVeli1987 <idrisveliev@gmail.com>
2023-12-26 21:06:39 +0400

    add styles

--- src/Pages/Project/index.jsx
+++ src/Pages/Project/index.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import Navigation from '../../Components/Navigation'
+import React from "react";
+import Navigation from "../../Components/Navigation";
+import { motion } from "framer-motion";
+import { Helmet } from "react-helmet";
+import PageBanner from "../../Components/PageBanner";
+import Footer from "../../Components/Footer";
+import Projects from "../../Components/Project";
 
 const Project = () => {
   return (
-    <div>
-      <Navigation/>
-    </div>
-  )
-}
+    <motion.div initial={{ opacity: 0 }} whileInView={{ opacity: 1 }}>
+      <Helmet>
+        <title>Project</title>
+      </Helmet>
+      <Navigation />
+      <PageBanner
+        src="../Project/Banner.svg"
+        page="Project"
+        title="Our Project"
+      />
+      <Projects />
+      <Footer />
+    </motion.div>
+  );
+};
 
-export default Project
+export default Project;
