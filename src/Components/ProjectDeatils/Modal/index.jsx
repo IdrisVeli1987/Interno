c6e790a19849d8b75e1bb20e9c8bbd03b307225e
IdrisVeli1987 <idrisveliev@gmail.com>
2024-01-11 17:09:39 +0400

    fix Modal issue

--- src/Components/ProjectDeatils/Modal/index.jsx
+++ src/Components/ProjectDeatils/Modal/index.jsx
@@ -1,20 +1,24 @@
 import React, { useState } from "react";
 import styles from "./style.module.css";
-import { Button } from "bootstrap";
+import { HiXMark } from "react-icons/hi2";
 
-const Modal = () => {
+const Modal = ({onCloseModal}) => {
   const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
+ 
+ 
   return (
-    <div className={styles.ModalContent} show={show} onhide={handleShow} >
-      <h4></h4>
+    <div className={styles.ModalContent}>
+      <div className={styles.ModalHeader}>
+        <h3>Minimal Bedroom</h3>
+        <button onClick={onCloseModal}>
+          <HiXMark fontSize={"32px"} />
+        </button>
+      </div>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero maiores
-        sint totam ipsa, deserunt placeat illo rerum sunt, est porro
-        reprehenderit voluptatem aliquam molestias amet dicta consectetur,
-        quaerat modi nemo.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam
+        veritatis expedita quasi. Harum dolor, numquam soluta, expedita
+        asperiores odit illo quae facere, autem in porro pariatur quia vitae
+        tenetur quis!
       </p>
     </div>
   );
